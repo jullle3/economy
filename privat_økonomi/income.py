32452d5992a0ed7c55ee7c427bcfef0482e4c812
julle <juliankl1509@gmail.com>
2023-06-17 17:19:20 +0200

    bolig priser

---
+++ privat_økonomi/income.py
@@ -0,0 +1,39 @@
+TAX_RATE = 0.42
+
+
+class Income:
+    """Calculate income after all expenditures and more"""
+
+    def __init__(self, indkomst: int, renter=0, kørselsfradrag=0):
+        """
+        :param indkomst: Indkomst før skat, fradrag og uden pension
+        """
+
+        # TODO: Er der flere betydelige fradrag?
+        self.rente_fradrag = _calc_rente_fradrag(renter)
+        self.fradrag = self.rente_fradrag + kørselsfradrag
+        self.skattepligtig_indkomst = indkomst - self.fradrag
+        self.skat = int(self.skattepligtig_indkomst * TAX_RATE)
+        self.udbetalt = indkomst - self.skat
+        # Beregn den ekstra indkomst som følge af fradrag
+        skat_uden_fradrag = int(indkomst * TAX_RATE)
+        udbetalt_uden_fradrag = indkomst - skat_uden_fradrag
+        self.ekstra_indkomst = self.udbetalt - udbetalt_uden_fradrag
+        # Hvor mange procent af fradraget som ender som reel indkomst
+        self.fradrag_forhold_procent = self.ekstra_indkomst / self.fradrag if self.ekstra_indkomst else 0
+
+        ekstra_udgifter = renter
+
+        # Den reelle pris som eventuelle rentestigninger har kostet
+        self.gevinst = self.ekstra_indkomst - ekstra_udgifter
+        print("")
+
+
+def _calc_rente_fradrag(renter: int) -> int:
+    # TODO: Så simpelt kan det ikke beregnes :P
+    rente_fradrag = renter * 0.336
+    return int(rente_fradrag)
+
+
+if __name__ == "__main__":
+    Income(500_000, 48000)
--- privat_økonomi/simple_property.py
+++ privat_økonomi/simple_property.py
@@ -1,5 +1,8 @@
+from privat_økonomi.income import Income
 from privat_økonomi.loan import calculate_total_interest_paid
 from privat_økonomi.utils import pexit
+
+EXPECTED_YEARLY_INCREASE_PERCENT = 0.05
 
 
 class SimpleProperty:
@@ -11,12 +14,16 @@ class SimpleProperty:
         Renter
      """
 
-    def __init__(self, bolig_pris, månedlig_bolig_udgift, years, income):
+    def __init__(self, pris, månedlig_bolig_udgift, years, income: int, _type: str):
         """Alle beregninger er i år, med mandre andet er angivet"""
-        self.bolig_pris = bolig_pris
+        self.years = years
+        self.type = _type
+        self.bolig_pris = pris
+        self.forventet_fremtidig_pris = self.calc_expected_property_value()
+        self.forventet_pristtigning = self.forventet_fremtidig_pris - self.bolig_pris
         # Antager man ligger minimums udbetaling på 5%
-        self.udbetaling = bolig_pris * 0.05
-        self.lån = bolig_pris * 0.95
+        self.udbetaling = pris * 0.05
+        self.lån = pris * 0.95
         self.renter_total = calculate_total_interest_paid(self.lån, 6, 10)
         self.renter = self.renter_total // years
         self.stiftelsesomkostninger = 10000
@@ -25,15 +32,25 @@ class SimpleProperty:
         # https://domstol.dk/tinglysningsretten/tingboegerne/tinglysningsafgifter/
         # 1.45% af det pantsikrede beløb
         self.tinglysningsafgift = self.lån * 0.0145 + 1660
-
         self.engangs_omkostninger = self.stiftelsesomkostninger + self.tinglysningsafgift
 
-        #
-        self.skattefradrag = self.renter
-        # print(f'Total: {int(self.månedlig_ydelse_total)}/måned')
+        self.income = Income(income, self.renter)
+        self.prisstigninger = 0
+        self.gevinst_efter_skat = self.income.gevinst + self.prisstigninger
+
+    def calc_expected_property_value(self):
+        return int(self.bolig_pris * (1 + EXPECTED_YEARLY_INCREASE_PERCENT) ** self.years)
+
+
+    def __str__(self):
+        return f'Pris {self.bolig_pris:<20} \n' \
+               f'Forventet årlig prisstigning {EXPECTED_YEARLY_INCREASE_PERCENT}% = \n' \
+               f'Indkomst over 10 år {self.gevinst_efter_skat}'
 
 
 YEARS = 10
-andels_bolig = SimpleProperty(1_150_000, 3000, YEARS)
+print(SimpleProperty(1_150_000, 3000, YEARS, 500_000, 'andel'))
+exit()
+andels_bolig = SimpleProperty(1_150_000, 3000, YEARS, 500_000)
 ny_andels_bolig = SimpleProperty(3_000_000, 3000, YEARS)
 ejer_bolig = SimpleProperty(3_000_000, 3000, YEARS)
